dc77efed0f1d9e6b7973c59a58d8a51aaee21557
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-22 23:07:08 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <main>
+      <section className="search">
+        <form action="" className="search-form">
+          <input type="text" placeholder="search" />
+          <button type="submit" className="submit-btn">
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+    </main>
   );
 }
 
