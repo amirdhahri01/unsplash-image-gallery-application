583a10e3cb8b14d99f2b5c975699b04423e222b8
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-23 22:47:56 +0100

    fetchimges function is done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photos/Photo";
-const clientId = "?client_id=ecJci9pc_gxMPU6SnVqK1r5RX6LOVBujpf_9lJC96Ms";
+const clientID = "?client_id=ecJci9pc_gxMPU6SnVqK1r5RX6LOVBujpf_9lJC96Ms";
 const mainUrl = "https://api.unsplash.com/photos/";
 const searchUrl = "https://api.unsplash.com/search/photos/";
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url ;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    if(query){
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    }else{
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+  }
   return (
     <main>
       <section className="search">
