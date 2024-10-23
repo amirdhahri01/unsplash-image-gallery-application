cea610ea5fe334c84189ce93ace1135735e810af
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-23 23:02:48 +0100

    Infinite scorlling functionality is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photos/Photo";
+
 const clientID = "?client_id=ecJci9pc_gxMPU6SnVqK1r5RX6LOVBujpf_9lJC96Ms";
 const mainUrl = "https://api.unsplash.com/photos/";
 const searchUrl = "https://api.unsplash.com/search/photos/";
+
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
@@ -13,15 +15,45 @@ function App() {
 
   const fetchImages = async () => {
     setLoading(true);
-    let url ;
+    let url;
     const urlPage = `&page=${page}`;
     const urlQuery = `&query=${query}`;
-    if(query){
-      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
-    }else{
-      url = `${mainUrl}${clientID}${urlPage}`
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
     }
-  }
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return { ...oldPhotos, ...data.results };
+        } else {
+          return { ...oldPhotos, ...data };
+        }
+      });
+    } catch (err) {
+      setLoading(false);
+      console.log(err.message());
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => oldPage + 1);
+      }
+    });
+    return window.removeEventListener("scroll", event);
+  });
   return (
     <main>
       <section className="search">
@@ -34,13 +66,12 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos center">
-          {photos.map((photo, index) => {
-            <Photo {...photo} key={index} />;
-          })}
+          {photos.map((photo, index) => (
+            <Photo {...photo} key={index} />
+          ))}
         </div>
       </section>
     </main>
   );
 }
-
 export default App;
